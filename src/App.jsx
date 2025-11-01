b554532e6eebe5c302fa2acd971a13fcf0144f63
Yash <yashwadkar079@gmail.com>
2025-11-01 19:27:35 +0530

    Remove Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import "./App.css";
-// import Navbar from "./components/Navbar";
 import WeatherDashboard from "./components/WeatherDashboard";
 
 function App() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-blue-400 to-purple-600">
-      {/* <Navbar /> */}
       <div className="container mx-auto px-4 py-6 max-w-7xl">
         <WeatherDashboard />
       </div>
